36887537b91e2236be262d95924d7d208a1e913e
Benjamín Ubach <bubach@homeaway.com>
2019-09-20 02:37:55 -0500

    Edit user!
    Signed-off-by: Benjamín Ubach <bubach@homeaway.com>

---
+++ handlers/user/{id}.js
@@ -0,0 +1,22 @@
+const User = require('../../models/User');
+
+module.exports = {
+  put: async function editUser(request, h) {
+    const { id: userId } = request.params;
+    const { fullName, username, password, isDriver, team } = request.payload;
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return h.response().code(404);
+    }
+    user.username = username;
+    user.fullName = fullName;
+    user.isDriver = isDriver;
+    user.team = team;
+    if (password) {
+      user.password = password;
+    }
+    await user.save();
+    return h.response(user).code(201);
+  }
+};
